47286c735bebd1ea1b8e9e958aa4befdaa0379d5
DoJunggeun <53655903+DoJunggeun@users.noreply.github.com>
2019-08-17 15:54:51 +0900

    Add files via upload

--- meal.js
+++ meal.js
@@ -13,13 +13,24 @@ function hgmeal2() {
 
 function hgmeal() {
   var date = new Date();
-  console.log(date.getDay(), date.getHours(), date.getMinutes(), date.getDate());
   if ((date.getDay() == 0 || date.getDay() == 6) && ((date.getHours() >= 17 && date.getHours() < 19) || (date.getHours() >= 12 && date.getHours() < 14) || (date.getHours() == 11 && date.getMinutes() > 30))) {
     return '<span style="color:blue;font-weight:bold;">운영중<span>';
   } else if ((date.getDay() > 0 && date.getDay() < 6) && ((date.getHours() >= 8 && date.getHours() < 10) || (date.getHours() >= 11 && date.getHours() < 16) || (date.getHours() >= 17 && date.getHours() < 19) || (date.getHours() == 19 && date.getMinutes() < 30))) {
     return '<span style="color:blue;font-weight:bold;">운영중<span>';
-  } else {
-    return '<span style="color:red;">운영종료<span>';
+  } else if (date.getDay() > 0 && date.getDay() < 6) {
+    if (date.getHours() < 8) {
+      return '<span style="color:red;">운영종료<br />'+gett(8,0)+'분남음<span>';
+    } else if (date.getHours() >= 10 && date.getHours() < 11) {
+      return '<span style="color:red;">운영종료<br />'+gett(11,0)+'분남음<span>';
+    } else if (date.getHours() >= 16 && date.getHours() < 17) {
+      return '<span style="color:red;">운영종료<br />'+gett(17,0)+'분남음<span>';
+    }
+  } else if (date.getDay() == 0 || date.getDay() == 6) {
+    if (date.getHours() <= 11) {
+      return '<span style="color:red;">운영종료<br />'+gett(11,30)+'분남음<span>';
+    } else if (date.getHours() >= 14 && date.getHours() < 17) {
+      return '<span style="color:red;">운영종료<br />'+gett(17,0)+'분남음<span>';
+    }
   }
 }
 
@@ -143,5 +154,21 @@ function ggmeal() {
     return '<span style="color:blue;font-weight:bold;">운영중<span>';
   } else {
     return '<span style="color:red;">운영종료<span>';
+  }
+}
+
+function gett(hour, minute){
+  var h = hour;
+  var m = minute;
+  var date = new Date();
+  var geth = hour - date.getHours();
+  var getm = minute - date.getMinutes();
+  if (geth < 0) {
+    return '오늘은끝';
+  } else if (getm < 0) {
+    getm = getm + 60;
+    geth = geth - 1;
+    getm = getm + geth * 60;
+    return getm;
   }
 }
