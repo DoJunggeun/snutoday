cf639a00087405a0a4901b300c3bb79a97956ae1
DoJunggeun <53655903+DoJunggeun@users.noreply.github.com>
2019-08-18 07:07:53 +0900

    Add files via upload

--- conv.js
+++ conv.js
@@ -4,7 +4,9 @@ function hgconv() {
     return '<span style="color:blue;font-weight:bold;">운영중<br/>' + gett(19, 0) + '분후종료</span>';
   } else if ((date.getDay() > 0 && date.getDay() < 6) && (date.getHours() >= 8 && date.getHours() < 20)) {
     return '<span style="color:blue;font-weight:bold;">운영중<br/>' + gett(20, 0) + '분후종료</span>';
-  } else if (date.getHours() < 8) {
+  } else if ((date.getDay() == 0 || date.getDay() == 6) && date.getHours() < 10) {
+    return '<span style="color:red;">미운영</span><br /><span style="color:green;font-weight:bold;">' + gett(10, 0) + '분후시작</span>';
+  } else if ((date.getDay() > 0 && date.getDay() < 6) && date.getHours() < 8) {
     return '<span style="color:red;">미운영</span><br /><span style="color:green;font-weight:bold;">' + gett(8, 0) + '분후시작</span>';
   } else {
     return '<span style="color:red;">운영종료<br />오늘은끝</span>';
