5df60b4fb95c8b619f39c3e40c61ae35615d1136
DoJunggeun <53655903+DoJunggeun@users.noreply.github.com>
2019-08-18 10:27:44 +0900

    Add files via upload

--- meal.js
+++ meal.js
@@ -51,13 +51,12 @@ function gongmeal() {
   } else if (date.getHours() >= 11 && date.getHours() < 19) {
     return '<span style="color:blue;font-weight:bold;">운영중<span>';
   } else if (date.getHours() < 11) {
-    return '<span style="color:red;">운영종료</span><br /><span style="color:green;font-weight:bold;">11시부터<br/>운영시작</span>';
+    return '<span style="color:red;">미운영</span><br /><span style="color:green;font-weight:bold;">11시부터<br/>운영시작</span>';
   } else {
     return '<span style="color:red;">운영종료<br />오늘은끝</span>';
   }
 }
 
-/*   이 아래로 ?분후시작 처리 안함. 기숙사 302 301 예술계  감골 + ?분후 종료는 다 안함ㅋ*/
 
 function dormmeal() {
   var date = new Date();
@@ -67,10 +66,16 @@ function dormmeal() {
     return '<span style="color:blue;font-weight:bold;">운영중<span>';
   } else if (date.getMinutes() >= 30 && ((date.getHours() == 11) || (date.getHours() == 17) || (date.getDay() > 0 && date.getDay() < 6 && date.getHours() == 7))) {
     return '<span style="color:blue;font-weight:bold;">운영중<span>';
+  } else if ((date.getHours() == 9 && date.getMinutes() >= 30) || (date.getHours() == 10) || (date.getHours() == 11 && date.getMinutes() < 30)) {
+    return '<span style="color:red;">미운영</span><br /><span style="color:green;font-weight:bold;">11시30분부터<br/>점심시작</span>';
+  } else if ((date.getHours() == 13 && date.getMinutes() >= 30) || (date.getHours() > 13 && date.getHours() < 17) || (date.getHours() == 17 && date.getMinutes() < 30)) {
+    return '<span style="color:red;">미운영</span><br /><span style="color:green;font-weight:bold;">17시30분부터<br/>저녁시작</span>';
   } else {
-    return '<span style="color:red;">미운영<span>';
+    return '<span style="color:red;">운영종료</span>';
+
   }
 }
+
 
 function eng302meal() {
   var date = new Date();
@@ -78,8 +83,12 @@ function eng302meal() {
     return '<span style="color:red;">평일에만<br />운영</span>';
   } else if ((date.getHours() > 11 && date.getHours() < 14) || (date.getHours() == 11 && date.getMinutes() >= 15) || (date.getHours() >= 17 && date.getHours() < 19)) {
     return '<span style="color:blue;font-weight:bold;">운영중<span>';
+  } else if (date.getHours() < 11 || (date.getHours() == 11 && date.getMinutes() < 15)) {
+    return '<span style="color:red;">미운영</span><br /><span style="color:green;font-weight:bold;">11시15분부터<br/>점심시작</span>';
+  } else if (date.getHours() < 17 && date.getHours() >= 14) {
+    return '<span style="color:red;">미운영</span><br /><span style="color:green;font-weight:bold;">17시부터<br/>저녁시작</span>';
   } else {
-    return '<span style="color:red;">미운영<span>';
+    return '<span style="color:red;">운영종료<span>';
   }
 }
 
@@ -89,11 +98,14 @@ function eng301meal() {
     return '<span style="color:red;">평일에만<br />운영</span>';
   } else if ((date.getHours() >= 11 && date.getHours() < 14) || (date.getHours() >= 17 && date.getHours() < 19)) {
     return '<span style="color:blue;font-weight:bold;">운영중<span>';
+  } else if (date.getHours() < 11) {
+    return '<span style="color:red;">미운영</span><br /><span style="color:green;font-weight:bold;">11시부터<br/>점심시작</span>';
+  } else if (date.getHours() < 17 && date.getHours() >= 14) {
+    return '<span style="color:red;">미운영</span><br /><span style="color:green;font-weight:bold;">17시부터<br/>저녁시작</span>';
   } else {
-    return '<span style="color:red;">미운영<span>';
+    return '<span style="color:red;">운영종료<span>';
   }
 }
-
 
 function artmeal() {
   var date = new Date();
@@ -101,8 +113,12 @@ function artmeal() {
     return '<span style="color:red;">평일에만<br />운영</span>';
   } else if ((date.getHours() > 11 && date.getHours() < 14) || (date.getHours() == 11 && date.getMinutes() >= 30) || (date.getHours() >= 17 && date.getHours() < 19)) {
     return '<span style="color:blue;font-weight:bold;">운영중<span>';
+  } else if (date.getHours() < 11 || (date.getHours() == 11 && date.getMinutes() < 30)) {
+    return '<span style="color:red;">미운영</span><br /><span style="color:green;font-weight:bold;">11시30분부터<br/>점심시작</span>';
+  } else if (date.getHours() < 17 && date.getHours() >= 14) {
+    return '<span style="color:red;">미운영</span><br /><span style="color:green;font-weight:bold;">17시부터<br/>저녁시작</span>';
   } else {
-    return '<span style="color:red;">미운영<span>';
+    return '<span style="color:red;">운영종료<span>';
   }
 }
 
@@ -113,6 +129,10 @@ function ggmeal() {
     return '<span style="color:red;">평일에만<br />운영</span>';
   } else if ((date.getHours() >= 11 && date.getHours() < 14) || (date.getHours() >= 17 && date.getHours() < 18) || (date.getHours() == 18 && date.getMinutes() < 30)) {
     return '<span style="color:blue;font-weight:bold;">운영중<span>';
+  } else if (date.getHours() < 11 ) {
+    return '<span style="color:red;">미운영</span><br /><span style="color:green;font-weight:bold;">11시부터<br/>점심시작</span>';
+  } else if (date.getHours() < 17 && date.getHours() >= 14) {
+    return '<span style="color:red;">미운영</span><br /><span style="color:green;font-weight:bold;">17시부터<br/>저녁시작</span>';
   } else {
     return '<span style="color:red;">미운영<span>';
   }
@@ -146,10 +166,10 @@ function m11141719() {
   } else if ((date.getHours() >= 11 && date.getHours() < 14) || (date.getHours() >= 17 && date.getHours() < 19)) {
     return '<span style="color:blue;font-weight:bold;">운영중<span>';
   } else if (date.getHours() < 11) {
-    return '<span style="color:red;">운영종료</span><br /><span style="color:green;font-weight:bold;">' + gett(11, 0) + '분남음</span>';
+    return '<span style="color:red;">미운영</span><br /><span style="color:green;font-weight:bold;">11시부터<br/>점심시작</span>';
   } else if (date.getHours() < 17 && date.getHours() >= 14) {
-    return '<span style="color:red;">운영종료</span><br /><span style="color:green;font-weight:bold;">' + gett(17, 0) + '분남음</span>';
+    return '<span style="color:red;">미운영</span><br /><span style="color:green;font-weight:bold;">17시부터<br/>저녁시작</span>';
   } else {
-    return '<span style="color:red;">운영종료<br />오늘은끝</span>';
+    return '<span style="color:red;">운영종료</span>';
   }
 }
