d38438aa3b157f32d5dc46c5c4936ca66b9efadc
DoJunggeun <53655903+DoJunggeun@users.noreply.github.com>
2019-08-19 18:19:52 +0900

    Add files via upload

--- functions.js
+++ functions.js
@@ -7,3 +7,11 @@ function gett(hour, minute) {
   getm = getm + geth * 60;
   return getm;
 }
+
+function gwangst() {
+  if (confirm("신청 기간은 8월 24 오전 9시 ~ 8월 26일 오후 11시입니다. 공지사항을 잘 읽고 신청해주세요!") == true) { //확인
+    window.open('https://docs.google.com/forms/d/e/1FAIpQLScNfOk8xp3oJQRgLuxRTLSF36TOTrXLPeg5akLLnBZ5CPmTog/viewform?fbclid=IwAR2DD0Z_cLd5A8EcpduefazS3ypJtG69OrV6jYisRgtHsQGUpx58-JMyjYg');
+  } else { //취소
+    return false;
+  }
+}
